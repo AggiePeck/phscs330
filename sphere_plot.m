24ff2bd8839540312d1e6505f6d687a891095caf
AggiePeck <134556312+AggiePeck@users.noreply.github.com>
2025-12-14 21:14:04 -0700

    Refactor to add sphere_plot function
    Replaced the sphere contraction demo with a function to visualize spherically symmetric density on a sphere.

--- sphere_plot.m
+++ sphere_plot.m
@@ -1,51 +1,25 @@
-% sphere_contraction_demo.m
-clear; close all; clc
+function sphere_plot(rho_mean)
+% visualize spherically symmetric density on a sphere
 
-% time vector
-Nt  = 200;
-t   = linspace(0,20,Nt);
+nTheta = 40;
+nPhi   = 80;
 
-% make a unit sphere grid
-[nx,ny,nz] = sphere(60);          % base coordinates for radius = 1
+theta = linspace(0,pi,nTheta);
+phi   = linspace(0,2*pi,nPhi);
+[TH,PH] = meshgrid(theta,phi);
 
-% initial radius and figure
-R0 = 1;
+R = 1.0;
+X = R.*sin(TH).*cos(PH);
+Y = R.*sin(TH).*sin(PH);
+Z = R.*cos(TH);
+
+rho_ang = rho_mean * ones(size(X));  % same density everywhere
 
 figure;
-ax = axes;
-axis equal
-hold(ax,'on');
+surf(X,Y,Z,rho_ang,'EdgeColor','none');
+axis equal;
 xlabel('x'); ylabel('y'); zlabel('z');
-view(40,25);
-
-% some dummy "density" perturbation that changes in time
-% here it is just a traveling wave on the surface
-theta = acos(nz);                 % polar angle
-phi   = atan2(ny,nx);             % azimuth
-
-for k = 1:Nt
-
-    % radius oscillates in time (contraction / expansion)
-    R = R0*(1 + 0.1*sin(2*pi*t(k)/max(t)));
-
-    % update coordinates
-    X = R*nx;
-    Y = R*ny;
-    Z = R*nz;
-
-    % color map from a surface perturbation (you can use your rho' or v')
-    surf_mode = sin(theta).*cos(phi - 0.5*t(k));
-    % normalize colors
-    C = (surf_mode - min(surf_mode(:)))/(max(surf_mode(:)) - min(surf_mode(:)));
-
-    if k == 1
-        h = surf(ax,X,Y,Z,C);
-        shading interp
-        colormap jet
-        colorbar
-    else
-        set(h,'XData',X,'YData',Y,'ZData',Z,'CData',C);
-    end
-
-    drawnow;
+colormap(turbo);
+colorbar;
+title('Spherically symmetric density');
 end
